423e883e81b1edf8f40827a04d383c02c73832aa
dcf-01 <rkezunovski98@gmail.com>
2021-05-13 01:02:27 +0200

    chaned Mailer, now uses separate node app

--- Utils/Mailer.cs
+++ Utils/Mailer.cs
@@ -8,6 +8,9 @@ using Amazon.SimpleEmail;
 using Amazon;
 using Amazon.SimpleEmail.Model;
 using System.Web;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace semenarna_id2 {
     public class Mailer {
@@ -21,67 +24,31 @@ namespace semenarna_id2 {
             htmlBody = html;
         }
 
-        // Replace sender@example.com with your "From" address.
-        // This address must be verified with Amazon SES.
+        public static string Base64Encode(string plainText) {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
 
-        // Replace recipient@example.com with a "To" address. If your account
-        // is still in the sandbox, this address must be verified.
-
-        // The configuration set to use for this email. If you do not want to use a
-        // configuration set, comment out the following property and the
-        // ConfigurationSetName = configSet argument below. 
-        static readonly string configSet = "ConfigSet";
-
-        // The subject line for the email.
-        static readonly string subject = "Amazon SES test (AWS SDK for .NET)";
-
-        // The email body for recipients with non-HTML email clients.
-        static readonly string textBody = "Amazon SES Test (.NET)\r\n"
-                                        + "This email was sent through Amazon SES "
-                                        + "using the AWS SDK for .NET.";
+        public class JsonContent : StringContent {
+            public JsonContent(object obj) :
+                base(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json") { }
+        }
 
 
+        public void Send() {
 
-        
+            var client = new HttpClient();
 
-         public void Send() {
-            // Replace USWest2 with the AWS Region you're using for Amazon SES.
-            // Acceptable values are EUWest1, USEast1, and USWest2.
-            using (var client = new AmazonSimpleEmailServiceClient(RegionEndpoint.EUCentral1)) {
-                var sendRequest = new SendEmailRequest {
-                    Source = senderAddress,
-                    Destination = new Destination {
-                        ToAddresses =
-                        new List<string> { receiverAddress }
-                    },
-                    Message = new Message {
-                        Subject = new Content(subject),
-                        Body = new Body {
-                            Html = new Content {
-                                Charset = "UTF-8",
-                                Data = htmlBody
-                            },
-                            Text = new Content {
-                                Charset = "UTF-8",
-                                Data = textBody
-                            }
-                        }
-                    },
-                };
-                try {
-                    Console.WriteLine("Sending email using Amazon SES...");
-                    var response = client.SendEmailAsync(sendRequest).GetAwaiter().GetResult();
-                    Console.WriteLine("The email was sent successfully.");
-                }
-                catch (Exception ex) {
-                    Console.WriteLine("The email was not sent.");
-                    Console.WriteLine("Error message: " + ex.Message);
+            var base64Html = Base64Encode(htmlBody);
 
-                }
-            }
+            string uri = "http://localhost:3000/send";
 
-            /*Console.Write("Press any key to continue...");
-            Console.ReadKey();*/
+            var message = new { from = senderAddress, to = receiverAddress, subject = "random subject", html = base64Html };
+
+
+
+            client.PostAsync(uri, new JsonContent(message));
+
         }
     }
 }
