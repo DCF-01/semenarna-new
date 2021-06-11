51ac68d568a4b51dc0df53c48a1b89c47a398793
dcf-01 <rkezunovski98@gmail.com>
2021-06-12 01:08:29 +0200

    removed hsts

--- Startup.cs
+++ Startup.cs
@@ -84,7 +84,7 @@ namespace semenarna_id2 {
             else {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+               /* app.UseHsts();*/
             }
             app.UseForwardedHeaders(new ForwardedHeadersOptions {
                 ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
